e8e399cf1babdfa846a208656474487d99146fff
Bruno Winkeler <bruno.winkeler@virtus.ufcg.edu.br>
2021-07-30 18:43:59 -0300

    Adicionar primeiro conteúdo dinâmico à página html

--- jogoteca.py
+++ jogoteca.py
@@ -4,6 +4,6 @@ app = Flask(__name__)
 
 @app.route('/inicio')
 def ola():
-    return render_template('lista.html')
+    return render_template('lista.html', titulo='Jogos') # Jinja2 é a resposável por colocar conteúdo dinâmico nas páginas html
 
 app.run()
