f2fd6b9d9e45288c2f5cd7a44b3b89d4bdc50403
Bruno Winkeler <brunowinkeler@gmail.com>
2021-07-31 12:47:19 -0300

    Adicionar session e flash messages no login

--- jogoteca.py
+++ jogoteca.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session, flash
 
 app = Flask(__name__)
+app.secret_key = 'alura'
 
 class Jogo:
     def __init__(self, nome, categoria, console):
@@ -43,8 +44,11 @@ def login():
 @app.route('/autenticar', methods=['POST',])
 def autenticar():
     if 'mestra' == request.form['senha']:
+        session['usuario_logado'] = request.form['usuario']
+        flash(request.form['usuario'] + ' logou com sucesso!')
         return redirect('/')
     else:
+        flash('Não foi possível logar. Tente novamente!')
         return redirect('/login')
 
 
